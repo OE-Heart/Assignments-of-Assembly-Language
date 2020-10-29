77aa88c24a2d74722f54f850fab23bfa320e848f
OE-Heart <leoouyixin@126.com>
2020-10-29 10:42:24 +0800

    update code

--- Assignment1/3190104783欧翌昕.asm
+++ Assignment1/3190104783欧翌昕.asm
@@ -1,90 +1,75 @@
-data segment
-    s db 100 dup(0)
-    t db 100 dup(0)
-data ends
+date segment
+s db 100 dup(0)
+t db 100 dup(0)
+date ends
 
 code segment
-assume cs:code, ds:data
+assume cs:code, ds:date
 
 shift:
-    sub al, 20h
-    mov t[di], al
-    add di, 1
-    jmp transfer_loop
+	sub al, 20h
+	mov [t+di], al
+	add di, 1
+	jmp scan_loop
 
 main:
-    mov ax, data
-    mov ds, ax
-    mov si, 0
-    mov di, 0
+	mov ax, date
+	mov ds, ax
+	mov si, 0
+	mov di, 0
 
 scan_loop:
-    mov ah, 01h
-    int 21h
-    
-    cmp al, 0Dh
-    je scan_end
+	mov ah, 01h
+	int 21h	
 
-    mov s[si], al
-    add si, 1
-    jmp scan_loop
+	cmp al, 0Dh
+	je scan_end
+
+	mov [s+si], al
+	add si, 1
+	
+	cmp al, 20h
+	je scan_loop
+
+	cmp al, 61h
+	jnb shift
+	
+	mov [t+di], al
+	add di, 1
+	jmp scan_loop
 
 scan_end:
-    mov s[si], 00h
-
-    mov ah, 02h
-    mov dl, 0Dh
-    int 21h
-
-    mov ah, 02h
-    mov dl, 0Ah
-    int 21h
-
-    mov bx, offset s
-    mov si, 0
-
-transfer_loop:
-    mov al, [bx+si]
-    cmp al, 00h
-    je transfer_end
-
-    cmp al, 20h
-    je transfer_loop
-
-    cmp al, 61h
-    jnb shift
-
-    mov t[di], al
-    add di, 1
-    jmp transfer_loop
-
-transfer_end:
-    mov t[di], 00h
-
-    mov bx, offset t
-    mov di, 0
+	mov [s+si], 00h
+	mov [t+di], 00h
+	
+	mov ah, 02h
+	mov dl, 0Dh
+	int 21h	
+	mov dl, 0Ah
+	int 21h	
+	
+	mov bx, offset t
+	mov di, 0
 
 print_loop:
-    mov dl, [bx+di]
-    cmp dl, 00h
-    je print_end
+	mov dl, [bx+di]
+	cmp dl, 00h
+	je print_end
 
-    mov ah, 02h
-    int 21h
-    add di, 1
-    jmp print_loop
+	mov ah, 02h
+	int 21h	
+	add di, 1
+	jmp print_loop
 
 print_end:
-    mov ah, 02h
-    mov dl, 0Dh
-    int 21h
+	mov ah, 02h
+	mov dl, 0Dh
+	int 21h	
+	mov dl, 0Ah
+	int 21h	
 
-    mov ah, 02h
-    mov dl, 0Ah
-    int 21h
+	mov ah, 4ch
+	int 21h
 
-    mov ah, 4ch
-    int 21h
-    
 code ends
-end main                         
+end main
