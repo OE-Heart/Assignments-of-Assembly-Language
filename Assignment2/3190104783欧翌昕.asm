520458233de392eea09dd39e30d7f9432232b8cf
OE-Heart <leoouyixin@126.com>
2020-11-02 11:32:29 +0800

    update code

--- Assignment2/3190104783欧翌昕.asm
+++ Assignment2/3190104783欧翌昕.asm
@@ -1,8 +1,20 @@
-data segment
-
-data ends
-
 code segment
-assume assume cs:code, ds:data
+assume assume cs:code
+
+main:
+    mov ax, data
+    mov ds, ax
+    mov ax, 0B800h
+    mov es, ax
+    mov al, 00h
+    mov ah, 0Ch
+    mov cx, 11
+column_loop:
+    mov dx, 25
+    mov di, 0
+row_loop:
+    mov byte ptr es:[di], ax
+
 
 code ends
+end main
